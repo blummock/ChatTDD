8f7299cd707ebad9fcb801a51529ee498f972e77
sergei <@email>
2026-08-04 16:18:56 +0300

    ExceptionMapper fix

--- app/src/main/java/com/blummock/chattdd/chat_feature/data/mappers/ExceptionMapper.kt
+++ app/src/main/java/com/blummock/chattdd/chat_feature/data/mappers/ExceptionMapper.kt
@@ -3,7 +3,7 @@ package com.blummock.chattdd.chat_feature.data.mappers
 import com.blummock.chattdd.chat_feature.domain.entity.DomainError
 import java.io.IOException
 
-class ExceptionMapper {
+internal class ExceptionMapper {
 
     fun map(exception: Throwable): DomainError = when (exception) {
         is IOException -> DomainError.NoInternet
