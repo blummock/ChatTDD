18472e0fc0724575877c433678dff1e4a39cfa03
sergei <@email>
2026-08-04 15:06:23 +0300

    TimeConverterTest fix

--- app/src/test/java/com/blummock/chattdd/chat_feature/core/TimeConverterTest.kt
+++ app/src/test/java/com/blummock/chattdd/chat_feature/core/TimeConverterTest.kt
@@ -9,13 +9,13 @@ class TimeConverterTest {
     @Test
     fun `scenario with default timezone`() {
         val timeConverter = TimeConverter.Base()
-        assertEquals("11:32", timeConverter.toHHmm("1785843165123L"))
+        assertEquals("11:32", timeConverter.toHHmm(1785843165123L))
     }
 
     @Test
     fun `scenario with custom timezone`() {
         val timeConverter = TimeConverter.Base(TimeZone.of("+03:00"))
-        assertEquals("14:32", timeConverter.toHHmm("1785843165123L"))
+        assertEquals("14:32", timeConverter.toHHmm(1785843165123L))
     }
 
 }
